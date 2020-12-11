4bdf078dac725b5c7806c4c79bb4db3d31fe8307
Roni Hokkanen <roni.hokkanen@protonmail.com>
2020-12-11 14:17:28 +0200

    Add day 10

--- common.nim
+++ common.nim
@@ -1,13 +1,19 @@
 import os, strutils
 
-proc getInput*(filename = var string): seq[string] =
-  if paramCount != 1:
-    filename = "input.txt"
-  else:
-    filename = paramStr(1)
+proc getInput*(filename = "input.txt"): seq[string] =
+  let filename =
+    if paramCount() == 1:
+      paramStr(1)
+    else:
+      filename
   for line in lines(getCurrentDir() & '/' & filename):
     result.add(line)
 
 proc getInputAsInt*(filename = "input.txt"): seq[int] =
+  let filename =
+    if paramCount() == 1:
+      paramStr(1)
+    else:
+      filename
   for line in lines(getCurrentDir() & '/' & filename):
     result.add parseInt(line)
